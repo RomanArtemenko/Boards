4914ab5971e51d122960a96d0390bbaf59b91c2a
Roman Artemenko <RGArtemenko@gmail.com>
2018-09-19 15:58:44 +0300

    added custom user model

--- boards/boards/settings.py
+++ boards/boards/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'custom-auth',
+    'custom_auth',
     'rest_framework',
 ]
 
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'custom_auth.CustomUser'
--- boards/custom_auth/models.py
+++ boards/custom_auth/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 # Create your models here.
+
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password=None, **extra_fields):
+        """
+         Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            ** extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+
+class CustomUser(AbstractUser):
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        verbose_name='username',
+        max_length=150,
+        validators=[username_validator]
+    )
+    email = models.EmailField(
+        verbose_name='email address',
+        unique=True,
+        error_messages={
+            'unique': ("A user with that email already exists."),
+        },
+    )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta(AbstractUser.Meta):
+        swappable = 'AUTH_USER_MODEL'
