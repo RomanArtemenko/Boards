e7a41176478e215b729fec1d07a8c00939df7174
Roman Artemenko <RGArtemenko@gmail.com>
2018-10-01 16:36:39 +0300

    added model status

--- boards/card/serializers.py
+++ boards/card/serializers.py
@@ -1,10 +1,19 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import Card
+from .models import Card, Status
+
+
+class StatusSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        mode = Status
+        fields = ('id', 'name')
 
 
 class CardSerializer(serializers.ModelSerializer):
+    # owner = serializers.
+    status = StatusSerializer
 
         class Meta:
             model = Card
-            fields = ('id', 'title', 'description', 'assigned_to', 'due_date', 'owner')
+            fields = ('id', 'title', 'description', 'assigned_to', 'due_date', 'owner', 'status')
