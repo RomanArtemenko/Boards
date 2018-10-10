8573e607b126a30b4e2099eecb87ff1125fb32a3
Roman Artemenko <RGArtemenko@gmail.com>
2018-10-10 10:18:17 +0300

    deleted folder with static

--- boards/boards/settings.py
+++ boards/boards/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/user/Документы/boards/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 try:
     from .social_config import OAUTH_CREDENTIALS
