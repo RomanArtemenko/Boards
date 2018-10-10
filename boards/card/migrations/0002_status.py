1d31da47fd696ade7ace55e99cd86b660594f4c9
Roman Artemenko <RGArtemenko@gmail.com>
2018-10-10 15:41:20 +0300

    some chenges in migration file

--- boards/card/migrations/0002_status.py
+++ boards/card/migrations/0002_status.py
@@ -8,7 +8,7 @@ import os
 
 def load_data(apps, schema_editor):
 
-    print('>>>>>>>>> CUR DIR : %s' % os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    print('>>>>>>>>> CUR DIR : %s' % os.path.dirname(os.path.abspath(__file__)))
     file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '/../', '/boards'))
     print('>>>> FILE PATH : %s' % file_path)
     file_name = 'status_data.json'
