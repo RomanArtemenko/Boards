811e79503fabe7052c6b8695cff9328e286a7ee1
Roman Artemenko <RGArtemenko@gmail.com>
2018-10-16 15:07:13 +0300

    added path to static in url

--- boards/urls.py
+++ boards/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -24,4 +26,4 @@ urlpatterns = [
     url('', include('social_django.urls', namespace='social')),
     url(r'^api/login/', include('rest_social_auth.urls_token')),
     path('manage/', include('card.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
